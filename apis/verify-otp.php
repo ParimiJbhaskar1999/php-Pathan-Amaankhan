af7b887d4f13f6d2378bdd50357844399774863e
Amaan Khan <amaanpathan5@gmail.com>
2021-06-15 15:18:02 +0530

    Added verify-otp api which can be integrated with front-end later on

--- apis/verify-otp.php
+++ apis/verify-otp.php
@@ -2,13 +2,13 @@
     require_once dirname( __DIR__ ) . '/classes/class-user.php';
     require_once dirname( __DIR__ ) . '/headers.php';
 
-    if( isset( $_POST['email']  ) && isset( $_POST['otp'] ) ) {
+    if ( isset( $_POST['email']  ) && isset( $_POST['otp'] ) ) {
         $email       = $_POST['email'];
         $otp         = $_POST['otp'];
-        $user        = new User( $email );
+        $user        = new User();
         $verified    = $user->verify_otp( $email, $otp );
 
-        if( $verified === 'success' ) {
+        if ( $verified === 'success' ) {
             $response = array(
                 'success' => true,
                 'message' => 'Verified successfully.',
