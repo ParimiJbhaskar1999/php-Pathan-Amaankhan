d53cddf90f001c37a9be9393415412e9e242c982
Amaan Khan <amaanpathan5@gmail.com>
2021-06-15 15:15:51 +0530

    Added send-verification-mail api which can be integrated with front-end later on

--- apis/send-verification-mail.php
+++ apis/send-verification-mail.php
@@ -2,12 +2,12 @@
     require_once dirname( __DIR__ ) . '/classes/class-user.php';
     require_once dirname( __DIR__ ) . '/headers.php';
 
-    if( isset( $_POST['email']  ) ) {
+    if ( isset( $_POST['email']  ) ) {
         $email       = $_POST['email'];
-        $user        = new User( $email );
-        $mail_send   = $user->send_verification_email();
+        $user        = new User();
+        $mail_send   = $user->send_verification_email( $email );
 
-        if( $mail_send === 'success' ) {
+        if ( $mail_send === 'success' ) {
             $response = array(
                 'success' => true,
                 'message' => 'Email send successfully.',
